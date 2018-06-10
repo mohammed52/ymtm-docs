e727bc330c551322cdcc34a8881e23f1ce35ac94
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2018-06-10 09:45:08 +0500

    updated api End Point for Heroku

--- config/app.js
+++ config/app.js
@@ -4,7 +4,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'https://door-quote-automator.herokuapp.com/';
+export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'https://ymtm-docs.herokuapp.com/';
 // export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'http://localhost:5000';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
