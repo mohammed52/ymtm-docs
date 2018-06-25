d922f3977c47a0ef430246ad994504e08f5984c7
Rashheh <rasheedaabbas@gmail.com>
2018-06-25 13:20:56 +0500

    wip: added timeout to keep server alive

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import initPassport from './init/passport';
 import initExpress from './init/express';
 import initRoutes from './init/routes';
 import renderMiddleware from './render/middleware';
+var http = require("http");
 require("babel-core/register");
 require("babel-polyfill");
 const app = express();
@@ -58,6 +59,12 @@ initRoutes(app);
  * renderMiddleware matches the URL with react-router and renders the app into
  * HTML
  */
+
+setInterval(function() {
+  http.get("http://ymtm-docs.herokuapp.com");
+  console.log("Staying Alive");
+}, 300000); // every 5 minutes (300000)
+
 app.get('*', renderMiddleware);
 
 app.listen(app.get('port'));
