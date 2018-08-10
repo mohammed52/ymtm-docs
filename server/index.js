93ff80287ed586b3b96293a121c74ed5245eb591
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2018-08-10 16:20:10 +0500

    wip: heroku setInterval

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import webpack from 'webpack';
-import { isDebug } from '../config/app';
-import { connect } from './db';
-import initPassport from './init/passport';
-import initExpress from './init/express';
-import initRoutes from './init/routes';
-import renderMiddleware from './render/middleware';
+import express from "express";
+import webpack from "webpack";
+import { isDebug } from "../config/app";
+import { connect } from "./db";
+import initPassport from "./init/passport";
+import initExpress from "./init/express";
+import initRoutes from "./init/routes";
+import renderMiddleware from "./render/middleware";
+var http = require("http");
 require("babel-core/register");
 require("babel-polyfill");
 const app = express();
-var cors = require('cors')
+var cors = require("cors");
 // using SendGrid's v3 Node.js Library
 
 /*
@@ -26,17 +27,19 @@ var cors = require('cors')
 
 if (isDebug) {
   // enable webpack hot module replacement
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const webpackConfig = require('../webpack/webpack.config');
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
+  const webpackConfig = require("../webpack/webpack.config");
   const devBrowserConfig = webpackConfig({
     browser: true
   });
   const compiler = webpack(devBrowserConfig);
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: devBrowserConfig.output.publicPath
-  }));
+  app.use(
+    webpackDevMiddleware(compiler, {
+      noInfo: true,
+      publicPath: devBrowserConfig.output.publicPath
+    })
+  );
   app.use(webpackHotMiddleware(compiler));
 }
 
@@ -50,7 +53,7 @@ initExpress(app);
  *
  * Note: Some of these routes have passport and database model dependencies
  */
-app.use(cors())
+app.use(cors());
 initRoutes(app);
 
 /*
@@ -59,6 +62,11 @@ initRoutes(app);
  * renderMiddleware matches the URL with react-router and renders the app into
  * HTML
  */
-app.get('*', renderMiddleware);
+app.get("*", renderMiddleware);
 
-app.listen(app.get('port'));
+setInterval(() => {
+  console.log("ymtm-docs pinging...");
+  http.get("https://ymtm-docs.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
+app.listen(app.get("port"));
