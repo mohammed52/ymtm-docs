69ac8ce532e1c5e6fb098d5cf13a180331d8832b
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2018-12-20 12:45:32 +0500

    removed package-lock.json

--- app/containers/ShowDocsContainer.jsx
+++ app/containers/ShowDocsContainer.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
-import { connect } from 'react-redux';
-import { browserHistory } from 'react-router';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+import { connect } from "react-redux";
+import { browserHistory } from "react-router";
 
-import docsListForProfile from './helpers/docsListForProfile'
-import docsListForApplicationGeneral from './helpers/docsListForApplicationGeneral'
-import docsListForApplicationSpecific from './helpers/docsListForApplicationSpecific'
-import { setSelectedOptions } from '../actions/selectedOptionsActions'
-import { DEFAULT_SETTINGS } from './helpers/defaultSettings'
-import SendEmailModal from './SendEmailModal'
-import EmailConfirmationModal from './EmailConfirmationModal'
-import { validateEmail } from './helpers/validateEmail'
-import { emailService } from '../services'
+import docsListForProfile from "./helpers/docsListForProfile";
+import docsListForApplicationGeneral from "./helpers/docsListForApplicationGeneral";
+import docsListForApplicationSpecific from "./helpers/docsListForApplicationSpecific";
+import { setSelectedOptions } from "../actions/selectedOptionsActions";
+import { DEFAULT_SETTINGS } from "./helpers/defaultSettings";
+import SendEmailModal from "./SendEmailModal";
+import EmailConfirmationModal from "./EmailConfirmationModal";
+import { validateEmail } from "./helpers/validateEmail";
+import { emailService } from "../services";
 
-import styles from '../css/components/showDocsStyles';
+import styles from "../css/components/showDocsStyles";
 
-var ReactBootstrap = require('react-bootstrap');
+var ReactBootstrap = require("react-bootstrap");
 var Button = ReactBootstrap.Button;
 var Modal = ReactBootstrap.Modal;
 var FormGroup = ReactBootstrap.FormGroup;
@@ -28,16 +28,16 @@ var FieldGroup = ReactBootstrap.FieldGroup;
 var Input = ReactBootstrap.Input;
 
 class ShowDocsContainer extends Component {
-
   constructor(props) {
     super(props);
     this.btnClickGoBack = this.btnClickGoBack.bind(this);
     this.closeSendEmailModal = this.closeSendEmailModal.bind(this);
-    this.closeEmailConfirmationModal = this.closeEmailConfirmationModal.bind(this);
+    this.closeEmailConfirmationModal = this.closeEmailConfirmationModal.bind(
+      this
+    );
     this.sendEmail = this.sendEmail.bind(this);
     this.btnClickSendList = this.btnClickSendList.bind(this);
     this.onEmailChange = this.onEmailChange.bind(this);
-
 
     this.state = {
       applicationDetails: this.props.selectedOptions,
@@ -45,7 +45,7 @@ class ShowDocsContainer extends Component {
       showEmailConfirmationModal: false,
       email: "",
       emailIsDiabled: true
-    }
+    };
   }
 
   componentDidMount() {}
@@ -56,20 +56,20 @@ class ShowDocsContainer extends Component {
 
   btnClickGoBack() {
     browserHistory.push({
-      pathname: '/'
+      pathname: "/"
     });
   }
 
   closeSendEmailModal() {
     this.setState({
       showSendEmailModal: false
-    })
+    });
   }
 
   closeEmailConfirmationModal() {
     this.setState({
       showEmailConfirmationModal: false
-    })
+    });
   }
 
   sendEmail() {
@@ -79,28 +79,29 @@ class ShowDocsContainer extends Component {
       selectedOptions: this.props.selectedOptions
     };
 
-    emailService().sendEmailWithDocs({
-      data
-    })
-      .then((res) => {
+    emailService()
+      .sendEmailWithDocs({
+        data
+      })
+      .then(res => {
         console.log("MAP response");
         console.log("res.status", res.status);
         console.log("res.data", res.data);
-
-      }).catch((e) => {
-      console.log(e);
-    });
+      })
+      .catch(e => {
+        console.log(e);
+      });
 
     this.setState({
       showSendEmailModal: false,
       showEmailConfirmationModal: true
-    })
+    });
   }
 
   btnClickSendList() {
     this.setState({
       showSendEmailModal: true
-    })
+    });
   }
 
   onEmailChange(event) {
@@ -109,107 +110,151 @@ class ShowDocsContainer extends Component {
     this.setState({
       email: event.target.value,
       emailIsDiabled: buttonIsDisbled
-    })
+    });
   }
-
 
   render() {
     console.log("this.props.selectedOptions", this.props.selectedOptions);
 
     let profileDocs = docsListForProfile(this.props.selectedOptions);
     console.log("profileDocs", profileDocs);
-    let applicationDocsGeneral = docsListForApplicationGeneral(this.props.selectedOptions);
+    let applicationDocsGeneral = docsListForApplicationGeneral(
+      this.props.selectedOptions
+    );
     console.log("applicationDocsGeneral", applicationDocsGeneral);
-    let applicationDocsSpecfic = docsListForApplicationSpecific(this.props.selectedOptions);
+    let applicationDocsSpecfic = docsListForApplicationSpecific(
+      this.props.selectedOptions
+    );
     console.log("applicationDocsSpecfic", applicationDocsSpecfic);
-
 
     let divArrProfileDocList = [];
     for (var i = 0; i < profileDocs.length; i++) {
       const sn = i + 1;
       divArrProfileDocList.push(
-        <div className={[styles.docsItem].join(' ')} key={"divArrProfileDocList" + i}>
-          {sn + ". " + profileDocs[i].docName + ' '}
+        <div
+          className={[styles.docsItem].join(" ")}
+          key={"divArrProfileDocList" + i}
+        >
+          {sn + ". " + profileDocs[i].docName + " "}
         </div>
-
-      )
+      );
     }
 
     let divArrGeneralDocList = [];
     for (var j = 0; j < applicationDocsGeneral.length; j++) {
       const sn = j + 1;
       divArrGeneralDocList.push(
-        <div className={[styles.docsItem].join(' ')} key={"divArrGeneralDocList" + j}>
-          {sn + ". " + applicationDocsGeneral[j].docName + ' '}
-          {applicationDocsGeneral[j].docTemplate !== null ?
-           <strong><a href={"/docs/" + applicationDocsGeneral[j].docTemplate}>(download template)</a></strong>
-           : ''}
+        <div
+          className={[styles.docsItem].join(" ")}
+          key={"divArrGeneralDocList" + j}
+        >
+          {sn + ". " + applicationDocsGeneral[j].docName + " "}
+          {applicationDocsGeneral[j].docTemplate !== null ? (
+            <strong>
+              <a href={"/docs/" + applicationDocsGeneral[j].docTemplate}>
+                (download template)
+              </a>
+            </strong>
+          ) : (
+            ""
+          )}
         </div>
-
-      )
+      );
     }
 
     let divArrSpecificDocList = [];
     for (var k = 0; k < applicationDocsSpecfic.length; k++) {
       const sn = k + 1 + j;
       divArrSpecificDocList.push(
-        <div className={[styles.docsItem].join(' ')} key={"divArrSpecificDocList" + k}>
-          {sn + ". " + applicationDocsSpecfic[k].docName + ' '}
-          {applicationDocsSpecfic[k].docTemplate !== null ?
-           <strong><a href={"/docs/" + applicationDocsSpecfic[k].docTemplate}>(download template)</a></strong>
-           : ''}
+        <div
+          className={[styles.docsItem].join(" ")}
+          key={"divArrSpecificDocList" + k}
+        >
+          {sn + ". " + applicationDocsSpecfic[k].docName + " "}
+          {applicationDocsSpecfic[k].docTemplate !== null ? (
+            <strong>
+              <a href={"/docs/" + applicationDocsSpecfic[k].docTemplate}>
+                (download template)
+              </a>
+            </strong>
+          ) : (
+            ""
+          )}
         </div>
-
-      )
+      );
     }
 
     return (
       <div className="container-fluid">
-        <div className={[styles.docsListWrapper, 'well'].join(' ')}>
-          {this.props.selectedOptions.REPEAT_APPLY === DEFAULT_SETTINGS.REPEAT_APPLY.TAG_FIRST_TIME_APPLY ?
-           <div>
-             <strong className={[styles.docsItem].join(' ')}>Documents required for your online profile (one time only):</strong>
-             <br/>
-             {divArrProfileDocList}
-             <br/>
-           </div> : ''}
-          <strong className={[styles.docsItem].join(' ')}>Documents required to be submitted with your Application:</strong>
-          <br/>
+        <div className={[styles.docsListWrapper, "well"].join(" ")}>
+          {this.props.selectedOptions.REPEAT_APPLY ===
+          DEFAULT_SETTINGS.REPEAT_APPLY.TAG_FIRST_TIME_APPLY ? (
+            <div>
+              <strong className={[styles.docsItem].join(" ")}>
+                Documents required for your online profile (one time only):
+              </strong>
+              <br />
+              {divArrProfileDocList}
+              <br />
+            </div>
+          ) : (
+            ""
+          )}
+          <strong className={[styles.docsItem].join(" ")}>
+            Documents required to be submitted with your Application:
+          </strong>
+          <br />
           {divArrGeneralDocList}
           {divArrSpecificDocList}
         </div>
-        <strong className={[styles.docsItem].join(' ')}>Send above documents to Hussain Bhai Ghani in Tawfeer Office or email to: mohammed.petiwala52@gmail.com</strong>
-        <br/>
-        <br/>
-        <button className="btn btn-primary" type="button" onClick={this.btnClickSendList}>
+        <strong className={[styles.docsItem].join(" ")}>
+          Send above documents to Ali Haider Bhai 0335-0270252 in Tawfeer Office
+          or email to: mohammed.petiwala52@gmail.com
+        </strong>
+        <br />
+        <br />
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={this.btnClickSendList}
+        >
           Send This List to my Email
         </button>
-        <br/>
-        <button className="btn btn-default" type="button" onClick={this.btnClickGoBack}>
+        <br />
+        <button
+          className="btn btn-default"
+          type="button"
+          onClick={this.btnClickGoBack}
+        >
           Go Back
         </button>
-        <br/>
-        <br/>
-        <SendEmailModal show={this.state.showSendEmailModal}
-                        onHide={this.closeSendEmailModal}
-                        sendEmail={this.sendEmail}
-                        onEmailChange={this.onEmailChange}
-                        emailIsDiabled={this.state.emailIsDiabled} />
-        <EmailConfirmationModal show={this.state.showEmailConfirmationModal} onHide={this.closeEmailConfirmationModal} />
+        <br />
+        <br />
+        <SendEmailModal
+          show={this.state.showSendEmailModal}
+          onHide={this.closeSendEmailModal}
+          sendEmail={this.sendEmail}
+          onEmailChange={this.onEmailChange}
+          emailIsDiabled={this.state.emailIsDiabled}
+        />
+        <EmailConfirmationModal
+          show={this.state.showEmailConfirmationModal}
+          onHide={this.closeEmailConfirmationModal}
+        />
       </div>
-      );
+    );
   }
 }
 
 ShowDocsContainer.propTypes = {
   selectedOptions: PropTypes.object.isRequired
-// topics: PropTypes.array.isRequired,
-// typing: PropTypes.func.isRequired,
-// createTopic: PropTypes.func.isRequired,
-// destroyTopic: PropTypes.func.isRequired,
-// incrementCount: PropTypes.func.isRequired,
-// decrementCount: PropTypes.func.isRequired,
-// newTopic: PropTypes.string
+  // topics: PropTypes.array.isRequired,
+  // typing: PropTypes.func.isRequired,
+  // createTopic: PropTypes.func.isRequired,
+  // destroyTopic: PropTypes.func.isRequired,
+  // incrementCount: PropTypes.func.isRequired,
+  // decrementCount: PropTypes.func.isRequired,
+  // newTopic: PropTypes.string
 };
 
 function mapStateToProps(state) {
@@ -217,16 +262,18 @@ function mapStateToProps(state) {
     selectedOptions: state.selectedOptions
     // topics: state.topic.topics,
     // newTopic: state.topic.newTopic
-
   };
 }
 
 // Read more about where to place `connect` here:
 // https://github.com/rackt/react-redux/issues/75#issuecomment-135436563
-export default connect(mapStateToProps, {
-  // createTopic,
-  // typing,
-  // incrementCount,
-  // decrementCount,
-  // destroyTopic
-})(ShowDocsContainer);
+export default connect(
+  mapStateToProps,
+  {
+    // createTopic,
+    // typing,
+    // incrementCount,
+    // decrementCount,
+    // destroyTopic
+  }
+)(ShowDocsContainer);
