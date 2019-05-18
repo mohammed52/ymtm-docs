a0f79bd572963fe071e51c312143db52beaf18a0
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2019-05-18 10:52:28 +0500

    wip: update business bank statement requirement

--- app/containers/helpers/docsListForApplicationGeneral.jsx
+++ app/containers/helpers/docsListForApplicationGeneral.jsx
@@ -18,9 +18,9 @@ export default function docsListForApplicationGeneral(applicationDetails) {
   if (
     applicationDetails.AMOUNT > 1500000 &&
     applicationDetails.AMOUNT <= 2500000
-    ) {
+  ) {
     generalDocList.push(DOCS_LIST.DOC_GUARANTOR_CNIC_3);
-}
+  }
 
   // personal expenses
   if (
@@ -29,22 +29,22 @@ export default function docsListForApplicationGeneral(applicationDetails) {
     (applicationDetails.SOURCE_OF_INCOME ===
       DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_JOB ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_HOME_BASED_INCOME_TUITION) &&
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_HOME_BASED_INCOME_TUITION) &&
     (applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_ZIARAT ||
       applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_TRAVEL ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_SCHOOL_UNI_FEES ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_SCHOOL_UNI_FEES ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_PROPERTY_PURCHASE ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_PROPERTY_PURCHASE ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_CAR_MOTORCYCLE ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_CAR_MOTORCYCLE ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_JAMAAT_WAJEBAAT_FMB_DUES ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_JAMAAT_WAJEBAAT_FMB_DUES ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_PERSONAL_WEDDING ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_PERSONAL_WEDDING ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_HOME_RENOVATION)
-    ) {
+        DEFAULT_SETTINGS.PURPOSE.TAG_HOME_RENOVATION)
+  ) {
     generalDocList.push(DOCS_LIST.DOC_PERSONAL_EXPENSES);
   }
 
@@ -54,29 +54,29 @@ export default function docsListForApplicationGeneral(applicationDetails) {
     (applicationDetails.SOURCE_OF_INCOME ===
       DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_JOB ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_HOME_BASED_INCOME_TUITION ||
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_HOME_BASED_INCOME_TUITION ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP ||
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP) &&
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP) &&
     (applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_NEW_BUSINESS ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_EXISTING_BUSINESS ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_EXISTING_BUSINESS ||
       applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_ZIARAT ||
       applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_TRAVEL ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_SCHOOL_UNI_FEES ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_SCHOOL_UNI_FEES ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_PROPERTY_PURCHASE ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_PROPERTY_PURCHASE ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_CAR_MOTORCYCLE ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_CAR_MOTORCYCLE ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_JAMAAT_WAJEBAAT_FMB_DUES ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_JAMAAT_WAJEBAAT_FMB_DUES ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_PERSONAL_WEDDING ||
+        DEFAULT_SETTINGS.PURPOSE.TAG_PERSONAL_WEDDING ||
       applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_HOME_RENOVATION)
-    ) {
+        DEFAULT_SETTINGS.PURPOSE.TAG_HOME_RENOVATION)
+  ) {
     generalDocList.push(DOCS_LIST.DOC_PERSONAL_EXPENSES);
   }
 
@@ -86,10 +86,10 @@ export default function docsListForApplicationGeneral(applicationDetails) {
     (applicationDetails.SOURCE_OF_INCOME ===
       DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP)
-    ) {
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP)
+  ) {
     generalDocList.push(DOCS_LIST.DOC_FINANCIALS);
-}
+  }
   // test comment
   // NTN returns for last two years
   if (
@@ -97,46 +97,46 @@ export default function docsListForApplicationGeneral(applicationDetails) {
     (applicationDetails.SOURCE_OF_INCOME ===
       DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP)
-    ) {
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP)
+  ) {
     generalDocList.push(DOCS_LIST.DOC_NTN_RETURNS);
-}
+  }
 
   // business bank statement for 6 months
   if (
-    applicationDetails.SOURCE_OF_INCOME ===
-    DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP ||
-    applicationDetails.SOURCE_OF_INCOME ===
-    DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP
-    ) {
+    applicationDetails.AMOUNT >= 500000 &&
+    (applicationDetails.SOURCE_OF_INCOME ===
+      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_PARTNERSHIP ||
+      applicationDetails.SOURCE_OF_INCOME ===
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_BUSINESS_SOLE_PROP)
+  ) {
     generalDocList.push(DOCS_LIST.DOC_BUSINESS_BANK_STATEMENT);
-}
+  }
 
   // personal bank statement
   if (
-    (applicationDetails.SOURCE_OF_INCOME ===
+    ((applicationDetails.SOURCE_OF_INCOME ===
       DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_JOB ||
       applicationDetails.SOURCE_OF_INCOME ===
-      DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_HOME_BASED_INCOME_TUITION) &&
-    (applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_ZIARAT ||
-      applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_TRAVEL ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_PERSONAL_WEDDING ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_SCHOOL_UNI_FEES ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_PROPERTY_PURCHASE ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_CAR_MOTORCYCLE ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_JAMAAT_WAJEBAAT_FMB_DUES ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_NEW_BUSINESS ||
-      applicationDetails.PURPOSE ===
-      DEFAULT_SETTINGS.PURPOSE.TAG_HOME_RENOVATION) || 
-    applicationDetails.AMOUNT <= 1000000
-
-    ) {
+        DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_HOME_BASED_INCOME_TUITION) &&
+      (applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_ZIARAT ||
+        applicationDetails.PURPOSE === DEFAULT_SETTINGS.PURPOSE.TAG_TRAVEL ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_PERSONAL_WEDDING ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_SCHOOL_UNI_FEES ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_PROPERTY_PURCHASE ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_CAR_MOTORCYCLE ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_JAMAAT_WAJEBAAT_FMB_DUES ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_NEW_BUSINESS ||
+        applicationDetails.PURPOSE ===
+          DEFAULT_SETTINGS.PURPOSE.TAG_HOME_RENOVATION)) ||
+    applicationDetails.AMOUNT <= 500000
+  ) {
     generalDocList.push(DOCS_LIST.DOC_PERSONAL_BANK_STATEMENT);
   }
 
@@ -144,9 +144,9 @@ export default function docsListForApplicationGeneral(applicationDetails) {
   if (
     applicationDetails.SOURCE_OF_INCOME ===
     DEFAULT_SETTINGS.SOURCE_OF_INCOME.TAG_JOB
-    ) {
+  ) {
     generalDocList.push(DOCS_LIST.DOC_SALARY_SLIP);
-}
+  }
 
-return generalDocList;
+  return generalDocList;
 }
