f634b911eda83e11df1c8c15e76471b1581df36c
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2021-01-23 18:52:42 +0500

    updated helpline number

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
 // import Navigation from '../containers/Navigation';
 // import Message from '../containers/Message';
-import styles from '../css/main';
-import ymtm from '../images/ymtm.png';
-import favicon from '../images/favicon.png';
+import styles from "../css/main";
+import ymtm from "../images/ymtm.png";
+import favicon from "../images/favicon.png";
 // import MEK from '../images/MEK.png';
 
-
-var ReactBootstrap = require('react-bootstrap');
+var ReactBootstrap = require("react-bootstrap");
 var Navbar = ReactBootstrap.Navbar;
 var NavItem = ReactBootstrap.NavItem;
 var NavDropdown = ReactBootstrap.NavDropdown;
@@ -19,7 +18,6 @@ var MenuItem = ReactBootstrap.MenuItem;
 // using SendGrid's v3 Node.js Library
 
 // const cx = classNames.bind(styles);
-
 
 /*
  * React-router's <Router> component renders <Route>'s
@@ -32,58 +30,55 @@ var MenuItem = ReactBootstrap.MenuItem;
  */
 // const App = ({children}) => {
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      cssHasLoaded: false
-    }
+      cssHasLoaded: false,
+    };
     this.handleLoad = this.handleLoad.bind(this);
   }
 
   componentDidMount() {
-
     console.log("AppContainer componentDidMount");
 
-    window.addEventListener('load', this.handleLoad);
+    window.addEventListener("load", this.handleLoad);
   }
   handleLoad() {
     console.log("handleLoad"); //  $ is available here
     this.setState({
-      cssHasLoaded: true
-    })
+      cssHasLoaded: true,
+    });
   }
 
   componentDidUpdate() {
     console.log("AppContainer componentDidUpdate");
-    const ss = document.styleSheets
+    const ss = document.styleSheets;
     console.log("ss.length", ss.length);
   }
 
   render() {
-
     return (
       <div>
-        {!this.state.cssHasLoaded ? <div/> :
-         <div className={styles.mainWrapper}>
-           <div className={styles.headerWrapper2}>
-             <div className={styles.headerWrapper}>
-               <img src={ymtm}
-                    width="90"
-                    height="90"
-                    className="img-fluid" />
-               <strong>Helpline: 0313-7590210</strong>
-             </div>
-           </div>
-           {this.props.children}
-         </div>}
+        {!this.state.cssHasLoaded ? (
+          <div />
+        ) : (
+          <div className={styles.mainWrapper}>
+            <div className={styles.headerWrapper2}>
+              <div className={styles.headerWrapper}>
+                <img src={ymtm} width="90" height="90" className="img-fluid" />
+                <strong>Helpline: 0300-2182932</strong>
+              </div>
+            </div>
+            {this.props.children}
+          </div>
+        )}
       </div>
     );
   }
 }
 
 App.propTypes = {
-  children: PropTypes.object
+  children: PropTypes.object,
 };
 
 export default App;
